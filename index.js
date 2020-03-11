65b37a3774acbf56170de2b1817bf1e260243a77
unclezap <zeb.blackwell@gmail.com>
2020-03-11 01:50:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("text").innerHTML = "This is really cool!"
+    // target.innerText = "This is really cool!"
+});
+
+console.log("fires first")
