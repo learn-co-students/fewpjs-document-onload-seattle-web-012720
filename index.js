51cbd79644eec9b771b7e8341c88556e31bbc66c
github username <github email address>
2020-03-12 16:28:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let paragraph = document.getElementById('text')
+    console.log(paragraph.innerHTML)
+    paragraph.innerHTML = "This is really cool!"
+})
+console.log("this is firing before the content is loaded.")
